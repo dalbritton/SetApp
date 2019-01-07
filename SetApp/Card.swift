139e709dd9d6e08a74b11cbb7594ce25e4da9bde
davida <davida@davids-macbook-pro.local>
2019-01-06 18:21:53 -0600

    First pass at rules

--- SetApp/Card.swift
+++ SetApp/Card.swift
@@ -77,7 +77,7 @@ class Card {
             }
             let attributes: [NSAttributedString.Key : Any] = [
                 .strokeColor : self.color!.uiColor(),
-                .strokeWidth : self.shading!.rawValue == "filled" ? -10 : 10,
+                .strokeWidth : self.shading!.rawValue == "filled" ? -7 : 7,
                 .foregroundColor : self.color!.uiColor().withAlphaComponent(self.shading!.rawValue == "striped" ? 0.15 : 1.0)
             ]
             self.attributedString = NSAttributedString(string:  symbolString, attributes: attributes)
--- SetApp/SetApp.swift
+++ SetApp/SetApp.swift
@@ -101,16 +101,43 @@ class SetApp {
         }
     }
     
-    func validate(for theCardSet: [Int]) -> Bool {
-        var success = false
-        if theCardSet.count == 3 {
-            for index in 0..<3 {
-                
-                
-                
+    func validate(for cardSet: [Int]) -> Bool {
+        var score = 0
+        if cardSet.count == 3 {
+            let card1 = board[cardSet[0]].card!
+            let card2 = board[cardSet[1]].card!
+            let card3 = board[cardSet[2]].card!
+            
+            //They all have the same number or have three different numbers
+            let pips: Set = [ card1.pipCount!.rawValue, card2.pipCount!.rawValue ]
+            if (card1.pipCount == card2.pipCount && card2.pipCount == card3.pipCount)
+                || (card1.pipCount != card2.pipCount && !pips.contains(card3.pipCount!.rawValue) )
+            { score += 1
+            }
+            
+            //They all have the same symbol or have three different symbols
+            let symbols: Set = [ card1.symbol!.rawValue, card2.symbol!.rawValue ]
+            if (card1.symbol == card2.symbol && card2.symbol == card3.symbol)
+                || (card1.symbol != card2.symbol && !symbols.contains(card3.symbol!.rawValue) )
+            { score += 1
+            }
+            
+            //They all have the same shading or have three different shadings
+            let shadings: Set = [ card1.shading!.rawValue, card2.shading!.rawValue ]
+            if (card1.shading == card2.shading && card2.shading == card3.shading)
+                || (card1.shading != card2.shading && !shadings.contains(card3.shading!.rawValue) )
+            { score += 1
+            }
+            
+            //They all have the same color or have three different colors
+            let colors: Set = [ card1.color!.rawValue, card2.color!.rawValue ]
+            if (card1.color == card2.color && card2.color == card3.color)
+                || (card1.color != card2.color && !colors.contains(card3.color!.rawValue) )
+            { score += 1
             }
         }
-        return success
+        
+        return score == 4
     }
     
 } //SetApp
